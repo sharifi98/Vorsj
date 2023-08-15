c4392f773a0914afb1546dbe7ecf982918d5b0d0
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-08-15 12:44:44 +0200

    Initial Commit

---
+++ Vorsj/VorsjApp.swift
@@ -0,0 +1,17 @@
+//
+//  VorsjApp.swift
+//  Vorsj
+//
+//  Created by Hossein Sharifi on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct VorsjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
