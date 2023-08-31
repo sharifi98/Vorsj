9ae151262034cd37630331fab6fa1328c1c9a205
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-08-31 15:53:44 +0200

    added a jeg har aldri game

---
+++ Vorsj/Jeg har aldri/JegHarAldri.swift
@@ -0,0 +1,18 @@
+//
+//  JegHarAldri.swift
+//  Vorsj
+//
+//  Created by Hossein Sharifi on 31/08/2023.
+//
+
+import SwiftUI
+
+struct JegHarAldri: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JegHarAldri()
+}
---
+++ Vorsj/Jeg har aldri/JegHarAldriListView.swift
@@ -0,0 +1,18 @@
+//
+//  JegHarAldriListView.swift
+//  Vorsj
+//
+//  Created by Hossein Sharifi on 31/08/2023.
+//
+
+import SwiftUI
+
+struct JegHarAldriListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JegHarAldriListView()
+}
