2214faca25bce88e88fdf287e07b80c8bf150606
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-09-15 14:38:30 +0200

    fixed the issue where the preview would not load. The fix was to remove the color paramter in HundredQuestionsListView buttonsdata.

--- Vorsj/100Spørsmål/HundredQuestionsListView.swift
+++ Vorsj/100Spørsmål/HundredQuestionsListView.swift
@@ -9,32 +9,29 @@ import SwiftUI
 
 struct HundredQuestionsListView: View {
     let buttonsData = [
-        ("5050.json", "50/50", "%", Color.red),
-        ("Volume1.json", "Utgave 1", "⭐️", Color.green),
-        ("Volume2.json", "Utgave 2", "💫", Color.blue),
-        ("Volume3.json", "Utgave 3", "🎊", Color.orange),
-        ("Volume4.json", "Utgave 4 (DRØY)", "🍺", Color.yellow),
-        ("Volume41.json", "Utgave 8 (DRØY) ", "👙", Color.yellow),
-        ("Volume5BI.json", "Utgave 5 (BI)", "🎓", Color.purple),
-        ("Volume6.json", "Utgave 69 (Sambucas)", "🥃", Color.yellow),
-        ("Volume7.json", "Utgave 7", "👽", Color.pink),
-        ("Kompliment.json", "Kompliment", "💕", Color.pink),
+        ("5050.json", "50/50", "%"),
+        ("Volume1.json", "Utgave 1", "⭐️"),
+        ("Volume2.json", "Utgave 2", "💫"),
+        ("Volume3.json", "Utgave 3", "🎊"),
+        ("Volume4.json", "Utgave 4 (DRØY)", "🍺"),
+        ("Volume41.json", "Utgave 8 (DRØY) ", "👙"),
+        ("Volume5BI.json", "Utgave 5 (BI)", "🎓"),
+        ("Volume6.json", "Utgave 69 (Sambucas)", "🥃"),
+        ("Volume7.json", "Utgave 7", "👽"),
+        ("Kompliment.json", "Kompliment", "💕"),
     ]
-    
-    //("Parleken.json", "Duoleken", "💃🏼", Color.gray),
-    
+
     let columns = [
         GridItem(.flexible(), spacing: 20),
         GridItem(.flexible(), spacing: 20)
     ]
-    
+
     var body: some View {
-        
         NavigationStack {
             List {
                 Section {
                     ForEach(0..<buttonsData.count, id: \.self) { index in
-                        let (filename, title, emoji, _) = buttonsData[index]
+                        let (filename, title, emoji) = buttonsData[index]
                         NavigationLink(destination: HundredView(filename: filename, title: title)) {
                             HStack {
                                 Text(emoji)
@@ -42,8 +39,6 @@ struct HundredQuestionsListView: View {
                             }
                         }
                     }
-                    .listRowBackground(Color(#colorLiteral(red: 0.1490196078, green: 0.1490196078, blue: 0.1607843137, alpha: 1)))
-
                 }
             }
             .toolbar {
@@ -62,51 +57,8 @@ struct HundredQuestionsListView: View {
             }
             .navigationTitle("")
             .navigationBarTitleDisplayMode(.inline)
-            
-            
-            
             ComposeArea()
         }
-        
-        /*
-         NavigationStack {
-         ScrollView {
-         
-         VStack {
-         Text("HUNDRE")
-         .foregroundColor(.secondary)
-         .font(.largeTitle)
-         Text("SPØRSMÅL.")
-         .foregroundColor(.pink)
-         .font(.largeTitle)
-         }
-         
-         LazyVGrid(columns: columns, spacing: 20) {
-         ForEach(buttonsData, id: \.0) { (filename, title, emoji, color) in
-         NavigationLink(destination: HundredView(filename: filename, title: title)) {
-         VStack {
-         Text(emoji)
-         .font(.largeTitle)
-         Text(title)
-         }
-         .padding()
-         .frame(minWidth: 100, maxWidth: .infinity, minHeight: 150, maxHeight: .infinity)
-         .background(.white)
-         .cornerRadius(10)
-         .foregroundColor(.black)
-         .overlay(
-         RoundedRectangle(cornerRadius: 10)
-         .stroke(Color.white.opacity(1), lineWidth: 3)
-         )
-         .scaleEffect(1.03) // Slight scaling to give the hovering effect
-         }
-         
-         }
-         }
-         .padding()
-         }
-         }
-         */
     }
 }
 
--- Vorsj/ModelData.swift
+++ Vorsj/ModelData.swift
@@ -7,20 +7,33 @@
 
 import Foundation
 
+var dataCache: [String: Data] = [:]
+
 
 func load<T: Decodable>(_ filename: String) -> T {
-    let data: Data
+    // If data is already cached, decode and return it
+    if let cachedData = dataCache[filename] {
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(T.self, from: cachedData)
+        } catch {
+            fatalError("Couldn't parse \(filename) from cache as \(T.self):\n\(error)")
+        }
+    }
 
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
 
+    let data: Data
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
+
+    // Cache the loaded data
+    dataCache[filename] = data
 
     do {
         let decoder = JSONDecoder()
